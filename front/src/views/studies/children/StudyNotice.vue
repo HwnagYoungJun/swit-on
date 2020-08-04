0ba1c7bb40b83f8b9099c13d95ba2f1ca4a72aa0
marrywill <kti91@likelion.org>
2020-08-04 15:41:13 +0900

    S03P13D101-14 | modified StudyPage logic

--- front/src/views/studies/children/StudyNotice.vue
+++ front/src/views/studies/children/StudyNotice.vue
@@ -1,16 +1,36 @@
 <template>
 	<div class="card-wrap">
-		<ArticleCard />
-		<ArticleCard />
-		<ArticleCard />
+		<ArticleCard
+			v-for="article in articles"
+			:key="article.id"
+			:article="article"
+		/>
+		<!-- <ArticleCard />
+		<ArticleCard /> -->
 	</div>
 </template>
 
 <script>
 import ArticleCard from '@/components/common/ArticleCard.vue';
+import { fetchNoticeArticles } from '@/api/articles';
 export default {
+	data() {
+		return {
+			articles: [],
+		};
+	},
 	components: {
 		ArticleCard,
+	},
+	methods: {
+		async fetchNotice() {
+			const studyId = this.$route.params.id;
+			const { data } = await fetchNoticeArticles(studyId);
+			this.articles = data;
+		},
+	},
+	created() {
+		this.fetchNotice();
 	},
 };
 </script>
--- front/src/views/studies/children/StudyQuestion.vue
+++ front/src/views/studies/children/StudyQuestion.vue
@@ -1,9 +1,45 @@
 <template>
-	<div>question</div>
+	<div class="card-wrap">
+		<ArticleCard
+			v-for="article in articles"
+			:key="article.id"
+			:article="article"
+		/>
+		<!-- <ArticleCard />
+		<ArticleCard /> -->
+	</div>
 </template>
 
 <script>
-export default {};
+import ArticleCard from '@/components/common/ArticleCard.vue';
+import { fetchQnaArticles } from '@/api/articles';
+export default {
+	data() {
+		return {
+			articles: [],
+		};
+	},
+	components: {
+		ArticleCard,
+	},
+	methods: {
+		async fetchQna() {
+			const studyId = this.$route.params.id;
+			const { data } = await fetchQnaArticles(studyId);
+			this.articles = data;
+		},
+	},
+	created() {
+		this.fetchQna();
+	},
+};
 </script>
-
-<style></style>
+<style lang="scss" scoped>
+.card-wrap {
+	display: flex;
+	flex-wrap: wrap;
+	@media screen and (max-width: 1500px) {
+		justify-content: center;
+	}
+}
+</style>
