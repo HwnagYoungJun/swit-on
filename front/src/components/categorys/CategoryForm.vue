5a70fd95133d9748f978972baadd7323a6cb9043
HwnagYoungJun <dudghkd4512@gmail.com>
2020-08-06 22:38:03 +0900

    S03P13D101-11 | data bind

--- front/src/components/categorys/CategoryForm.vue
+++ front/src/components/categorys/CategoryForm.vue
@@ -1,24 +1,17 @@
 <template>
 	<div>
 		<ul class="study-box">
-			<li class="study-li">
-				<StudyCard></StudyCard>
-			</li>
-			<li class="study-li">
-				<StudyCard></StudyCard>
-			</li>
-			<li class="study-li">
-				<StudyCard></StudyCard>
-			</li>
-			<li class="study-li">
-				<StudyCard></StudyCard>
+			<li :key="study.id" v-for="study in studies" class="study-li">
+				<StudyCard :study="study" />
 			</li>
 		</ul>
 	</div>
 </template>
 
 <script>
+import axios from 'axios';
 import StudyCard from '@/components/common/StudyCard.vue';
+
 export default {
 	components: {
 		StudyCard,
@@ -26,7 +19,62 @@ export default {
 	data() {
 		return {
 			testData: null,
+			baseURL: process.env.VUE_APP_API_URL,
+			studies: [],
 		};
+	},
+	props: {
+		lowerCategory: String,
+	},
+	watch: {
+		lowerCategory() {
+			this.studies = [];
+			this.fetchStudy();
+			console.log(this.lowerCategory);
+		},
+	},
+	methods: {
+		async fetchStudy() {
+			const { data } = await axios.get(`${this.baseURL}study`, {
+				params: {
+					lowercategory_id: this.lowerCategoryId(this.lowerCategory),
+				},
+			});
+			console.log(this.lowerCategoryId(this.lowerCategory));
+			this.studies = data;
+		},
+		lowerCategoryId(lowerCategoryString) {
+			switch (lowerCategoryString) {
+				case 'Django':
+					return 1;
+				case 'Spring':
+					return 2;
+				case 'Node.js':
+					return 3;
+				case 'Vue':
+					return 4;
+				case 'React':
+					return 5;
+				case 'html/CSS':
+					return 6;
+				case 'JS':
+					return 7;
+				case 'DB':
+					return 8;
+				case '기타':
+					return 9;
+				case '안드로이드':
+					return 10;
+				case 'IOS':
+					return 11;
+				case '플러터':
+					return 12;
+				case '리액트네이티브':
+					return 13;
+				case '파이썬':
+					return 15;
+			}
+		},
 	},
 };
 </script>
--- front/src/views/categorys/CategoryPage.vue
+++ front/src/views/categorys/CategoryPage.vue
@@ -1,12 +1,17 @@
 <template>
 	<div>
-		<CategoryNavForm></CategoryNavForm>
+		<CategoryNavForm />
 		<br />
-		<h1>{{ initName }}</h1>
+		<h1 @click="resetLowerCategory">{{ initName }}</h1>
 		<br />
-		<h2>전체 스터디</h2>
+		<LowerCategoryNavForm
+			@changeLower="changeLower"
+			:upperCategory="upperCategoryId"
+		/>
+		<br />
+		<h2>{{ lowerCategory }} 스터디</h2>
 		<hr />
-		<CategoryForm :categoryName="categoryName"></CategoryForm>
+		<CategoryForm :lowerCategory="lowerCategory" />
 	</div>
 </template>
 
@@ -14,14 +19,17 @@
 import { fetchStudies } from '@/api/studies';
 import CategoryForm from '@/components/categorys/CategoryForm.vue';
 import CategoryNavForm from '@/components/categorys/CategoryNavForm.vue';
+import LowerCategoryNavForm from '@/components/categorys/children/LowerCategoryNavForm.vue';
 export default {
 	components: {
 		CategoryForm,
 		CategoryNavForm,
+		LowerCategoryNavForm,
 	},
 	data() {
 		return {
 			studyItems: [],
+			lowerCategory: '전체',
 		};
 	},
 	methods: {
@@ -29,13 +37,55 @@ export default {
 			const { data } = await fetchStudies();
 			this.studyItems = data.studies;
 		},
+		changeLower(categoryName) {
+			this.lowerCategory = categoryName;
+		},
+		resetLowerCategory() {
+			this.lowerCategory = '전체';
+		},
 	},
 	computed: {
 		initName() {
 			return this.$route.params.UpperCategoryName;
 		},
+		upperCategoryId() {
+			var categoryId = 0;
+			var categoryArray = [
+				'웹',
+				'모바일',
+				'프로그래밍언어',
+				'AI',
+				'CS',
+				'언어',
+			];
+			switch (this.initName) {
+				case categoryArray[0]:
+					categoryId = 1;
+					break;
+				case categoryArray[1]:
+					categoryId = 2;
+					break;
+				case categoryArray[2]:
+					categoryId = 3;
+					break;
+				case categoryArray[3]:
+					categoryId = 4;
+					break;
+				case categoryArray[4]:
+					categoryId = 5;
+					break;
+				case categoryArray[5]:
+					categoryId = 6;
+					break;
+			}
+			return categoryId;
+		},
 	},
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+h1 {
+	display: inline;
+}
+</style>
