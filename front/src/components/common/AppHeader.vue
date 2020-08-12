60d982252fd7741f28d7000b772053316e29e002
yeonjudkwl <hyj9785@likelion.org>
2020-08-12 19:05:04 +0900

    S03P13D101-27 | searchBar component

--- front/src/components/common/AppHeader.vue
+++ front/src/components/common/AppHeader.vue
@@ -11,66 +11,7 @@
 					<img v-else src="@/assets/color.png" alt="logo" class="switon"
 				/></router-link>
 			</div>
-			<label for="search" class="a11y-hidden">search: </label>
-			<div
-				class="auto-complete"
-				@keyup.down="selectStudy('down')"
-				@keyup.up="selectStudy('up')"
-			>
-				<input
-					autocomplete="off"
-					v-model="searchData"
-					v-on:input="fetchAutoComplete"
-					type="search"
-					id="search"
-					placeholder="소모임을 검색하세요"
-				/>
-				<div
-					v-if="searchedStudyData || searchedUpperData || searchedLowerData"
-					class="searched-datas"
-				>
-					<p v-if="searchedStudyData">스터디</p>
-					<ul v-if="searchedStudyData">
-						<li
-							v-for="data in searchedStudyData"
-							:key="data.id"
-							@click="moveStudy(data.id)"
-							@keyup.enter="moveStudy(data.id)"
-							tabindex="0"
-						>
-							{{ data.name }}
-						</li>
-					</ul>
-					<hr
-						v-if="searchedStudyData && (searchedUpperData || searchedLowerData)"
-					/>
-					<p v-if="searchedUpperData">상위 카테고리</p>
-					<ul v-if="searchedUpperData">
-						<li
-							v-for="data in searchedUpperData"
-							:key="data.id"
-							@click="moveCategory(data.name)"
-							@keyup.enter="moveCategory(data.name)"
-							tabindex="0"
-						>
-							{{ data.name }}
-						</li>
-					</ul>
-					<hr v-if="searchedUpperData && searchedLowerData" />
-					<p v-if="searchedLowerData">하위 카테고리</p>
-					<ul v-if="searchedLowerData">
-						<li
-							v-for="data in searchedLowerData"
-							:key="data.id"
-							@click="moveStudy(data.id)"
-							@keyup.enter="moveStudy(data.id)"
-							tabindex="0"
-						>
-							{{ data.name }}
-						</li>
-					</ul>
-				</div>
-			</div>
+			<Search />
 		</div>
 		<nav class="nav-router">
 			<template v-if="!isUserLogin">
@@ -105,21 +46,17 @@
 <script>
 import { mapMutations, mapState } from 'vuex';
 import { baseAuth } from '@/api/index';
-import { searchStudy } from '@/api/studies.js';
-
-// 방향키 index
-let index = -1;
+import Search from '@/components/common/Search.vue';
 
 export default {
 	data() {
 		return {
-			searchData: '',
-			searchedUpperData: null,
-			searchedLowerData: null,
-			searchedStudyData: null,
 			userName: this.name ? this.name : null,
 			profileImg: null,
 		};
+	},
+	components: {
+		Search,
 	},
 	computed: {
 		...mapState(['name']),
@@ -148,57 +85,6 @@ export default {
 		},
 		onChangeSearch(val) {
 			this.searchData = val;
-		},
-		async fetchAutoComplete(e) {
-			try {
-				if (!e.target.value) {
-					this.searchedUpperData = null;
-					this.searchedLowerData = null;
-					this.searchedStudyData = null;
-					return;
-				}
-				const { data } = await searchStudy(e.target.value);
-
-				this.searchedUpperData = data.uppercategories.length
-					? data.uppercategories
-					: null;
-				this.searchedLowerData = data.lowercategories.length
-					? data.lowercategories
-					: null;
-				this.searchedStudyData = data.studies.length ? data.studies : null;
-			} catch (error) {
-				console.log(error);
-			}
-		},
-		clearSearchedData() {
-			this.searchData = '';
-			this.searchedUpperData = null;
-			this.searchedLowerData = null;
-			this.searchedStudyData = null;
-		},
-		moveStudy(id) {
-			this.clearSearchedData();
-			this.$router.push(`/study/${id}`);
-		},
-		moveCategory(name) {
-			this.clearSearchedData();
-			this.$router.push(`/category/${name}`);
-		},
-		// 방향키
-		selectStudy(key) {
-			const search = document.querySelector('#search');
-			let list = document.querySelectorAll('.searched-datas li');
-			let size = list.length - 1;
-			if (key === 'down' && index < size) {
-				index++;
-			} else if (key === 'up' && index > 0) {
-				index--;
-			} else {
-				search.focus();
-				index = -1;
-				return;
-			}
-			list[index].focus();
 		},
 	},
 	created() {
@@ -255,71 +141,12 @@ header {
 	.nav-logo {
 		@include scale(margin-right, 32px);
 	}
-	#search {
-		width: 400px;
-		padding: 0.8rem 2rem 0.8rem 0.75rem;
-		font-size: $font-light;
-		border: 1px solid #dbdbdb;
-		color: -internal-light-dark(black, white);
-		box-shadow: rgba(0, 0, 0, 0.04) 0px 0px 1px 0px,
-			rgba(41, 42, 43, 0.16) 0px 1px 3px 0px;
-		border-radius: 3px;
-		transition: all 0.35s ease;
-		&:focus {
-			outline: none;
-			background: rgb(248, 248, 249);
-			border: none;
-			width: 420px;
-			box-shadow: none;
-		}
-		@media screen and (max-width: 768px) {
-			width: 200px;
-			&:focus {
-				width: 220px;
-			}
-		}
-	}
 	.nav-search-icon {
 		position: absolute;
 		right: 0;
 		font-size: $font-bold;
 		color: -internal-light-dark(black, white);
 		margin: 0.5rem;
-	}
-	.auto-complete {
-		position: relative;
-		.searched-datas {
-			width: 420px;
-			position: absolute;
-			top: 60px;
-			background: #fff;
-			box-shadow: 0 3px 7px rgb(214, 214, 214);
-			z-index: 999;
-			p {
-				padding: 3px 10px;
-				font-size: $font-light;
-				font-weight: bold;
-			}
-			li {
-				padding: 10px;
-				&:hover {
-					background: #f2f2f2;
-					cursor: pointer;
-				}
-				&:focus {
-					background: #f2f2f2;
-					outline: none;
-				}
-			}
-			hr {
-				width: 95%;
-				border: none;
-				border-bottom: 1px solid #d1d1d1;
-			}
-			@media screen and (max-width: 768px) {
-				width: 220px;
-			}
-		}
 	}
 }
 .nav-router {
---
+++ front/src/components/common/Search.vue
@@ -0,0 +1,203 @@
+<template>
+	<div
+		class="auto-complete"
+		@keyup.down="selectStudy('down')"
+		@keyup.up="selectStudy('up')"
+	>
+		<label for="search" class="a11y-hidden">search: </label>
+		<input
+			autocomplete="off"
+			v-model="searchData"
+			v-on:input="fetchAutoComplete"
+			type="search"
+			id="search"
+			placeholder="소모임을 검색하세요"
+		/>
+		<div
+			v-if="searchedStudyData || searchedUpperData || searchedLowerData"
+			class="searched-datas"
+		>
+			<p v-if="searchedStudyData">스터디</p>
+			<ul v-if="searchedStudyData">
+				<li
+					v-for="data in searchedStudyData"
+					:key="data.id"
+					@click="moveStudy(data.id)"
+					@keyup.enter="moveStudy(data.id)"
+					tabindex="0"
+				>
+					{{ data.name }}
+				</li>
+			</ul>
+			<hr
+				v-if="searchedStudyData && (searchedUpperData || searchedLowerData)"
+			/>
+			<p v-if="searchedUpperData">상위 카테고리</p>
+			<ul v-if="searchedUpperData">
+				<li
+					v-for="data in searchedUpperData"
+					:key="data.id"
+					@click="moveCategory(data.name)"
+					@keyup.enter="moveCategory(data.name)"
+					tabindex="0"
+				>
+					{{ data.name }}
+				</li>
+			</ul>
+			<hr v-if="searchedUpperData && searchedLowerData" />
+			<p v-if="searchedLowerData">하위 카테고리</p>
+			<ul v-if="searchedLowerData">
+				<li
+					v-for="data in searchedLowerData"
+					:key="data.id"
+					@click="moveStudy(data.id)"
+					@keyup.enter="moveStudy(data.id)"
+					tabindex="0"
+				>
+					{{ data.name }}
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+import { searchStudy } from '@/api/studies.js';
+// 방향키 index
+let index = -1;
+
+export default {
+	data() {
+		return {
+			searchData: '',
+			searchedUpperData: null,
+			searchedLowerData: null,
+			searchedStudyData: null,
+		};
+	},
+	methods: {
+		async fetchAutoComplete(e) {
+			try {
+				if (!e.target.value) {
+					this.searchedUpperData = null;
+					this.searchedLowerData = null;
+					this.searchedStudyData = null;
+					return;
+				}
+				const { data } = await searchStudy(e.target.value);
+
+				this.searchedUpperData = data.uppercategories.length
+					? data.uppercategories
+					: null;
+				this.searchedLowerData = data.lowercategories.length
+					? data.lowercategories
+					: null;
+				this.searchedStudyData = data.studies.length ? data.studies : null;
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		clearSearchedData() {
+			this.searchData = '';
+			this.searchedUpperData = null;
+			this.searchedLowerData = null;
+			this.searchedStudyData = null;
+		},
+		moveStudy(id) {
+			this.clearSearchedData();
+			this.$router.push(`/study/${id}`);
+		},
+		moveCategory(name) {
+			this.clearSearchedData();
+			this.$router.push(`/category/${name}`);
+		},
+		// 방향키
+		selectStudy(key) {
+			const search = document.querySelector('#search');
+			let list = document.querySelectorAll('.searched-datas li');
+			let size = list.length - 1;
+			if (key === 'down' && index < size) {
+				index++;
+			} else if (key === 'up' && index > 0) {
+				index--;
+			} else {
+				search.focus();
+				index = -1;
+				return;
+			}
+			list[index].focus();
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+#search {
+	width: 400px;
+	padding: 0.8rem 2rem 0.8rem 0.75rem;
+	font-size: $font-light;
+	border: 1px solid #dbdbdb;
+	color: -internal-light-dark(black, white);
+	box-shadow: rgba(0, 0, 0, 0.04) 0px 0px 1px 0px,
+		rgba(41, 42, 43, 0.16) 0px 1px 3px 0px;
+	border-radius: 3px;
+	transition: all 0.35s ease;
+	&:focus {
+		outline: none;
+		background: rgb(248, 248, 249);
+		border: none;
+		width: 420px;
+		box-shadow: none;
+	}
+	@media screen and (max-width: 768px) {
+		width: 200px;
+		&:focus {
+			width: 220px;
+		}
+	}
+}
+.auto-complete {
+	position: relative;
+	.searched-datas {
+		width: 420px;
+		position: absolute;
+		top: 60px;
+		background: #fff;
+		box-shadow: 0 3px 7px rgb(214, 214, 214);
+		z-index: 999;
+		p {
+			padding: 3px 10px;
+			font-size: $font-light;
+			font-weight: bold;
+		}
+		li {
+			padding: 10px;
+			&:hover {
+				background: #f2f2f2;
+				cursor: pointer;
+			}
+			&:focus {
+				background: #f2f2f2;
+				outline: none;
+			}
+		}
+		hr {
+			width: 95%;
+			border: none;
+			border-bottom: 1px solid #d1d1d1;
+		}
+		@media screen and (max-width: 768px) {
+			width: 220px;
+		}
+	}
+}
+.a11y-hidden {
+	overflow: hidden;
+	position: absolute !important;
+	top: -9999em;
+	clip: rect(0, 0, 0, 0);
+	width: 1px;
+	height: 1px;
+	margin: -1px;
+}
+</style>
