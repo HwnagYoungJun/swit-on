8b6fe99ea701e36377023222c57d967f9f1604b4
yeonjudkwl <hyj9785@likelion.org>
2020-08-16 17:19:09 +0900

    S03P13D101-51 | main-description

--- front/src/views/Main.vue
+++ front/src/views/Main.vue
@@ -2,7 +2,7 @@
 	<section class="main-page">
 		<AppHeader></AppHeader>
 		<Search />
-		<div v-if="studies" class="popular-wrap">
+		<div v-if="studies && isUserLogin" class="popular-wrap">
 			<p class="popular-title">인기 소모임<span></span></p>
 			<router-link
 				class="popular-item-link"
@@ -13,6 +13,28 @@
 				<MainCard :study="study" colorPick="white" />
 			</router-link>
 		</div>
+		<div v-else class="main-page-description">
+			<div class="main-description-title">
+				<h1 class="animate__animated animate__zoomOut animate__delay-1s">
+					Study with Online
+				</h1>
+				<h1 class="animate__animated animate__zoomIn animate__delay-2s">
+					SwitOn
+				</h1>
+				<p>온라인에서 함께하는 스터디</p>
+			</div>
+			<p>
+				출석 버튼으로, 나도
+				<span>switch on</span><i class="icon ion-md-bulb high-light"></i>
+			</p>
+			<p>
+				정보 공유<i class="icon ion-md-information exclamation"></i> 일정 관리<i
+					class="icon ion-md-information exclamation"
+				></i>
+				이력 관리<i class="icon ion-md-information exclamation"></i>
+			</p>
+			<p>스윗온과 함께 해요 :)</p>
+		</div>
 	</section>
 </template>
 
@@ -21,6 +43,7 @@ import AppHeader from '@/components/common/AppHeader.vue';
 import Search from '@/components/common/Search.vue';
 import MainCard from '@/components/common/MainCard.vue';
 import { fetchStudies } from '@/api/studies';
+import { mapGetters } from 'vuex';
 
 export default {
 	components: {
@@ -33,6 +56,12 @@ export default {
 			studies: null,
 			isLoading: false,
 		};
+	},
+	computed: {
+		...mapGetters(['isLogin']),
+		isUserLogin() {
+			return this.isLogin;
+		},
 	},
 	methods: {
 		async fetchData() {
@@ -145,6 +174,72 @@ export default {
 		}
 		@media screen and (max-width: 1024px) and (min-width: 950px) {
 			width: 70%;
+		}
+	}
+	.main-page-description {
+		width: 100%;
+		min-height: 50vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		color: #fff;
+		.main-description-title {
+			display: grid;
+			place-items: center;
+			margin-bottom: 20px;
+			h1 {
+				font-size: 40px;
+			}
+			// .hide-show {
+			// 	animation: change-title 0.5s;
+			// 	-webkit-animation: change-title 0.5s;
+			// 	animation-delay: 1s;
+			// 	animation-fill-mode: forwards;
+			// }
+			// @keyframes change-title {
+			// 	from {
+			// 	}
+			// 	to {
+			// 		height: 0;
+			// 	}
+			// }
+			// @-webkit-keyframes change-title {
+			// 	from {
+			// 	}
+			// 	to {
+			// 		height: 0;
+			// 	}
+			// }
+		}
+		.exclamation {
+			margin: 0 10px 0 5px;
+			font-size: 1.5rem;
+			// transform: translateY(-15px) rotate(180deg);
+		}
+		.high-light {
+			margin: 0 5px;
+			font-size: 1.5rem;
+			animation: change-color 0.5s;
+			-webkit-animation: change-color 0.5s;
+			animation-delay: 3s;
+			animation-fill-mode: forwards;
+		}
+		@keyframes change-color {
+			from {
+				color: #fff;
+			}
+			to {
+				color: rgb(255, 217, 0);
+			}
+		}
+		@-webkit-keyframes change-color {
+			from {
+				color: #fff;
+			}
+			to {
+				color: rgb(255, 217, 0);
+			}
 		}
 	}
 }
